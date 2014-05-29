9eadf9d76baceb2f79d4594d482d5deb51bcdc4c
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-05-30 03:41:56 +0530

    Fix service endpoint setup form

--- lib/Drupal/fluxpocket/Plugin/Service/PocketService.php
+++ lib/Drupal/fluxpocket/Plugin/Service/PocketService.php
@@ -37,5 +37,22 @@ class PocketService extends OAuthServiceBase implements PocketServiceInterface {
       'service_url' => 'https://getpocket.com',
     ) + parent::getDefaultSettings();
   }
+  public function settingsForm(array &$form_state) {
+    $form['help'] = array(
+      '#type' => 'markup',
+      '#markup' => t('In the following, you need to provide authentication details for communicating with Pocket.<br/>For that, you have to create an application in the <a href="http://getpocket.com/developer/apps/">Pocket app management interface</a> and provide its consumer key below'),
+      '#prefix' => '<p class="fluxservice-help">',
+      '#suffix' => '</p>',
+      '#weight' => -1,
+    );
+    $form['consumer_key'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Consumer key'),
+      '#description' => t('The consumer key for authenticating through OAuth.'),
+      '#default_value' => $this->getConsumerKey(),
+      '#required' => TRUE,
+    );
+    return $form;
+  }
 
 }
