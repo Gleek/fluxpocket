30345e3b133b481c8237baa70cb25c96eecd14db
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-06-04 05:14:57 +0530

    Add Delete Rule

--- lib/Drupal/fluxpocket/Plugin/Rules/Action/DeleteUrl.php
+++ lib/Drupal/fluxpocket/Plugin/Rules/Action/DeleteUrl.php
@@ -15,50 +15,50 @@ use Duellsy\Pockpack\PockpackQueue;
 /**
  * Action for posting a status message on a page.
  */
-class AddUrl extends RulesPluginHandlerBase implements \RulesActionHandlerInterface {
+class DeleteUrl extends RulesPluginHandlerBase implements \RulesActionHandlerInterface {
 
   /**
    * Defines the action.
    */
   public static function getInfo() {
     return static::getInfoDefaults() + array(
-      'name' => 'fluxpocket_add_url',
-      'label' => t('Add Url to Pocket'),
+      'name' => 'fluxpocket_delete_url',
+      'label' => t('Delete Url from Pocket'),
       'parameter' => array(
         'page_url' => array(
           'type' => 'uri',
           'label' => t('Url'),
           'description' => t('The complete url. (e.g. http://www.example.com/page.html) or data-selector (eg. node:url in data selector mode)'),
         ),
-        'tags' => array(
-          'type' => 'text',
-          'label' => t('List of tags'),
-          'description' => t('List of tags seperated by comma (,)'),
-          '#required' => FALSE,
-        ),
         'account' => static::getAccountParameterInfo(),
       ),
     );
   }
 
+
   /**
    * Executes the action.
    */
-  public function execute($page_url, $tags, PocketAccountInterface $account) {
+  public function execute($page_url, PocketAccountInterface $account) {
     $client = $account->client();
     //Structuring the array of url to be added
-    //Getting Tag List
-    $tag_ar = explode(",",$tags);
-    //Trimming spaces from array list
-    $tag_ar = array_filter(array_map('trim', $tag_ar));
-    $link_info = array(
-      'url'       => $page_url,
-      'tags'      => $tag_ar
+    //Getting Url List
+    $options = array(
+    'state'         => 'all',
+    'detailType'    => 'simple'
     );
+    $list= $client->retrieve($options);
 
     $pockpack_q = new PockpackQueue();
-
-    $pockpack_q->add($link_info);
-    $client->send($pockpack_q);
+    //Calculate the item_id for the url
+    unset($id);
+    foreach($list->{'list'} as $index => $elements)
+      if($list->{'list'}->$index->{'given_url'} == $page_url)
+        $id = $list->{'list'}->$index->{'item_id'};
+    if(isset($id)){
+      $pockpack_q->delete($id);
+      $client->send($pockpack_q);
+    }
   }
+
 }
