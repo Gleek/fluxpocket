3f0e408d07ae57b6c55dc75eb00fc63ca6e36367
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-06-04 04:11:54 +0530

    Add tagging feature

--- lib/Drupal/fluxpocket/Plugin/Rules/Action/AddUrl.php
+++ lib/Drupal/fluxpocket/Plugin/Rules/Action/AddUrl.php
@@ -10,8 +10,6 @@ namespace Drupal\fluxpocket\Plugin\Rules\Action;
 use Drupal\fluxpocket\Plugin\Service\PocketAccount;
 use Drupal\fluxpocket\Plugin\Service\PocketAccountInterface;
 use Drupal\fluxpocket\Rules\RulesPluginHandlerBase;
-use Duellsy\Pockpack\Pockpack;
-use Duellsy\Pockpack\PockpackAuth;
 use Duellsy\Pockpack\PockpackQueue;
 
 /**
@@ -30,32 +28,42 @@ class AddUrl extends RulesPluginHandlerBase implements \RulesActionHandlerInterf
         'page_url' => array(
           'type' => 'uri',
           'label' => t('Url'),
-          'description' => t('The complete url. (e.g. http://www.example.com/page.html'),
+          'description' => t('The complete url. (e.g. http://www.example.com/page.html) or data-selector (eg. node:url in data selector mode)'),
         ),
-       'account' => static::getAccountParameterInfo(),
+        'tags' => array(
+          'type' => 'text',
+          'label' => t('List of tags'),
+          'description' => t('List of tags seperated by comma (,)'),
+          '#required' => FALSE,
+        ),
+        'account' => static::getAccountParameterInfo(),
       ),
     );
+  }
+
+  function fluxpocket_form_alter(&$form, &$form_state, $form_id) {
+   drupal_set_message($form_id);  // print form ID to messages
+   drupal_set_message(print_r($form, TRUE));  // print array to messages
   }
 
   /**
    * Executes the action.
    */
-  public function execute($page_url, PocketAccountInterface $account) {
+  public function execute($page_url, $tags, PocketAccountInterface $account) {
     $client = $account->client();
-    // Open a stream for reading and writing
-    $consumer_key = $_SESSION['fluxpocket-api']['consumer_key'];
-    #$request_token = $_SESSION['fluxpocket-api']['request_token'];
-    #drupal_goto("http://localhost/umar/testing/caught.php?con_key={$consumer_key}&req_token={$request_token}");
-    $access_token = $_SESSION['fluxpocket-api']['access_token'];
-    #$access_token = $client->receiveToken($consumer_key, $request_token);
+    //Structuring the array of url to be added
+    //Getting Tag List
+    $tag_ar = explode(",",$tags);
+    //Trimming spaces from array list
+    $tag_ar = array_filter(array_map('trim', $tag_ar));
     $link_info = array(
-      'url'       => $page_url
+      'url'       => $page_url,
+      'tags'      => $tag_ar
     );
 
-    $pockpack = new Pockpack($consumer_key, $access_token);
     $pockpack_q = new PockpackQueue();
 
     $pockpack_q->add($link_info);
-    $pockpack->send($pockpack_q);
+    $client->send($pockpack_q);
   }
 }
--- lib/Drupal/fluxpocket/PocketClient.php
+++ lib/Drupal/fluxpocket/PocketClient.php
@@ -9,6 +9,7 @@ use Guzzle\Common\Collection;
 use Guzzle\Plugin\Oauth\OauthPlugin;
 use Guzzle\Service\Client;
 use Duellsy\Pockpack\PockpackAuth;
+use Duellsy\Pockpack\Pockpack;
 
 /**
  * Service client for the Pocket API.
@@ -19,26 +20,9 @@ class PocketClient extends Client {
    * {@inheritdoc}
    */
   public static function factory($config = array()) {
-    $client = new PockpackAuth();
     $consumer_key = $config['client_id'];
     $access_token = $config['access_token'];
-    $_SESSION['fluxpocket-api']= array();
-    $_SESSION['fluxpocket-api']['consumer_key'] = $consumer_key;
-    /*if(!isset($_SESSION['fluxpocket-api']))
-      $_SESSION['fluxpocket-api']= array();
-    if(!isset($_SESSION['fluxpocket-api']['consumer_key']))
-      $_SESSION['fluxpocket-api']['consumer_key'] = $consumer_key;
-    if(!isset($_SESSION['fluxpocket-api']['request_token'])){
-      $request_token = $client->connect($consumer_key);
-      $_SESSION['fluxpocket-api']['request_token'] = $request_token;
-    }
-    if(!isset($_SESSION['fluxpocket-api']['access_token'])){
-      $redirect = null;
-      $request_token = $_SESSION['fluxpocket-api']['request_token'];
-      drupal_goto($path = "https://getpocket.com/auth/authorize?request_token={$request_token}&redirect_uri={$redirect}");
-      $access_token = $client->receiveToken($consumer_key, $request_token);*/
-      $_SESSION['fluxpocket-api']['access_token'] = $access_token;
-      // }
+    $client = new Pockpack($consumer_key, $access_token);
     return $client;
   }
 }
