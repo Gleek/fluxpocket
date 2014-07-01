77786022fb0735a6f5b1b4e33b6f1e0faa217833
Umar Ahmad <ahmad.umar2008@yahoo.in>
2014-07-01 20:15:37 +0530

    fix issue#5

--- lib/Drupal/fluxpocket/Tasks/PocketEntryTaskHandler.php
+++ lib/Drupal/fluxpocket/Tasks/PocketEntryTaskHandler.php
@@ -15,35 +15,54 @@ class PocketEntryTaskHandler extends PocketTaskBase {
    * {@inheritdoc}
    */
   public function runTask() {
-    $owner = $this->task['data']['owner'];
-
-    // Assemble the request arguments.
-    $arguments = array(
-      'state'         => 'all',
-      'detailType'    => 'complete'
-    );
-
     // Retrieve the timestamp of the last status update that was processed and
     // continue right there.
-    $store = fluxservice_key_value('fluxpocket.entries');
-    $arguments['since'] = $store->get($this->task['identifier']) ?: $this->task['date'];
-
+    $store = fluxservice_key_value('fluxpocket.entries.since');
+    $arguments = $this->getRequestArguments();
+    $identifier = $this->task['identifier'];
     $account = $this->getAccount();
-    $response = $account->client()->retrive($arguments);
-    $messages = $response->{'list'};
-    if (($response && $messages)){
-      foreach($messages as &$message) {
-        $message['type'] = 'entry';
-      }
-      $messages = fluxservice_entify_multiple($messages, 'fluxpocket_object', $account);
-      foreach ($messages as $message) {
-        rules_invoke_event($this->getEvent(), $account, $message, $owner);
+    $response = $account->client()->retrieve($arguments);
+    $entries = $response->{'list'};
+    if (($response && $entries)) {
+      $entries = fluxservice_entify_multiple($entries, 'fluxpocket_entry', $account);
+      foreach ($entries as $entry) {
+        rules_invoke_event($this->getEvent(), $account, $entry);
       }
 
       // Store the timestamp of the last status message that was processed.
-      $last = end($messages);
-      $store->set($this->task['identifier'], $last->getUpdatedTime());
+      $last = end($entries);
+      $store->set($this->task['identifier'], $last->getRemoteIdentifier());
     }
+    elseif (empty($arguments['since'])) {
+      $store->set($identifier, FALSE);
+    }
+  }
+
+  /**
+   * Retrieves the request arguments based on the event configuration.
+   *
+   * @return array
+   *   The request arguments.
+   */
+  protected function getRequestArguments() {
+    $arguments = array(
+      'state'         => 'all',
+      'detailType'    => 'complete'
+      'sort'          => 'oldest'
+    );
+    // We store the remote identifier of the last Pocket Entry that was
+    // processed so that we can benefit from the 'since_id' query argument.
+    $store = fluxservice_key_value('fluxpocket.entries.since');
+    if ($since_id = $store->get($this->task['identifier'])) {
+      $arguments['since'] = $since_id;
+    }
+    // If it hasn't been set yet, it means that we are running this for thes
+    // first time. In order to prevent flooding and processing of old Entries we
+    // limit the request to a single Entry.
+    elseif ($since_id === NULL) {
+      $arguments['count'] = 1;
+    }
+    return $arguments;
   }
 
 }
